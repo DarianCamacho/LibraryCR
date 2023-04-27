8475bff4442efb282fe00482982caaedf163443e
Darian Camacho <delgadodarian2003@gmail.com>
2023-04-27 02:04:28 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/LibrosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LibraryCR.Models;
+
+namespace LibraryCR.Controllers
+{
+    public class LibrosController : Controller
+    {
+        private LibreriaInternacionalEntities db = new LibreriaInternacionalEntities();
+
+        // GET: Libros
+        public ActionResult Index()
+        {
+            return View(db.Libros.ToList());
+        }
+
+        // GET: Libros/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libros libros = db.Libros.Find(id);
+            if (libros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libros);
+        }
+
+        // GET: Libros/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Libros/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ISBN,Titulo,Autor,FechaPublicacion,Precio,Foto")] Libros libros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Libros.Add(libros);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(libros);
+        }
+
+        // GET: Libros/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libros libros = db.Libros.Find(id);
+            if (libros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libros);
+        }
+
+        // POST: Libros/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ISBN,Titulo,Autor,FechaPublicacion,Precio,Foto")] Libros libros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(libros).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(libros);
+        }
+
+        // GET: Libros/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libros libros = db.Libros.Find(id);
+            if (libros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libros);
+        }
+
+        // POST: Libros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Libros libros = db.Libros.Find(id);
+            db.Libros.Remove(libros);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
